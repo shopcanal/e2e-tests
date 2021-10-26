1a188a35f7b0b6e5b532036b82e7c604d0438af3
alex-whitaker <alexwhitaker94@gmail.com>
2021-10-26 10:55:01 -0700

    test: try not running script every time

--- global-setup.ts
+++ global-setup.ts
@@ -1,12 +1,12 @@
-import { chromium, FullConfig } from '@playwright/test';
+import { FullConfig } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
   const dataSetupUrl = process.env.E2E_DATA_SETUP_URL;
   if (dataSetupUrl) {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page.goto(dataSetupUrl);
-    await browser.close();
+    // const browser = await chromium.launch();
+    // const page = await browser.newPage();
+    // await page.goto(dataSetupUrl);
+    // await browser.close();
   } else {
     console.log('No URL for data setup provided - skipping this step.');
   }
