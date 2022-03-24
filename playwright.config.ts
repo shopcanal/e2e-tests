3543a46431fe110359c030d30b5d52a933e9131d
Dylan Gattey <dylan.gattey@gmail.com>
2022-03-24 01:22:12 -0700

    chore: test 4 minute timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,8 @@ const config: PlaywrightTestConfig = {
   // Runs before the entire test suite
   globalSetup: require.resolve('./global-setup'),
 
-  // Minute for each test should be enough. When many tests are run at once, some take up to 45 seconds
-  timeout: 60000,
+  // 4 minute for each test to really stress test
+  timeout: 240000,
 
   use: {
     // Capture screenshots of failed runs
